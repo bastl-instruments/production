cd564640720a73f95b52aa11a6d8530acfb15e8f
LnnrtS <Lennart.Schierling@gmx.de>
2014-12-07 17:28:00 +0100

    added progammer as variable

--- osc_noise.sh
+++ osc_noise.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-avrdude -pm328p -carduino -P"$1" -v -v -Uflash:w:osc_noise.hex:a
+
+PROGRAMMER="/dev/ttyUSB0"
+
+
+avrdude -pm328p -carduino -P$PROGRAMMER -v -v -Uflash:w:osc_noise.hex:a
+
+read -rsp $'Press any key or wait 5 seconds to continue...\n' -n 1 -t 5;
